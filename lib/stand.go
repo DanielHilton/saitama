c53cdd646a6c5978a02726ffdc2bab0855e8a864
Daniel Hilton <daniellawrencehilton@hotmail.co.uk>
2019-05-23 22:15:25 +0100

    Some basic loops to run 'attacks'

--- lib/stand.go
+++ lib/stand.go
@@ -1,23 +1,40 @@
 package star_platinum
 
-import "fmt"
+import (
+	"fmt"
+	//"net/http"
+	"time"
+)
 
 type Stand struct {
 	Endpoint  string
 	Frequency int
+	attacking bool
 }
 
 func NewStand(endpoint string, frequency int) *Stand {
 	return &Stand{
 		endpoint,
 		frequency,
+		false,
 	}
 }
 
-func Attack(s Stand) {
-	go requestEndpoint(s.Endpoint)
+func (s Stand) StartAttacking() {
+	s.attacking = true
+	for s.attacking {
+		go requestEndpoint(s.Endpoint)
+		time.Sleep(time.Duration(1/s.Frequency) * time.Second)
+	}
+}
+
+func (s Stand) StopAttacking() {
+	s.attacking = false
 }
 
 func requestEndpoint(e string) {
 	fmt.Println("ORA ORA ORA ORA")
+
+	//resp, _ := http.Get(e)
+	//fmt.Println(resp)
 }
--- star-platinum.go
+++ star-platinum.go
@@ -3,9 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/DanielHilton/star-platinum/lib"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	signals := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
+
 	endpoint := flag.String("endpoint", "http://localhost", "The endpoint you want to attack")
 	standQuantity := flag.Int("quantity", 1, "The amount of stands you want to attack your endpoint with")
 
@@ -14,4 +22,33 @@ func main() {
 	notice := fmt.Sprintf("Attacking %s endpoint with %d stands", *endpoint, *standQuantity)
 	fmt.Println(notice)
 
+	stands := assembleStands(*endpoint, *standQuantity)
+
+	for _, stand := range stands {
+		go stand.StartAttacking()
+	}
+
+	go waitForSignal(signals, done)
+	<-done
+
+	for _, stand := range stands {
+		go stand.StopAttacking()
+	}
+	fmt.Println("Exiting")
+}
+
+func waitForSignal(sigChan <-chan os.Signal, done chan<- bool) {
+	sig := <-sigChan
+	fmt.Println(sig)
+	done <- true
+}
+
+func assembleStands(endpoint string, quantity int) []*star_platinum.Stand {
+	var stands []*star_platinum.Stand
+
+	for i := 0; i < quantity; i++ {
+		stands = append(stands, star_platinum.NewStand(endpoint, 1))
+	}
+
+	return stands
 }
